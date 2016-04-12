80937456270bb8eed5369017e4dc5efc1b73148e
Jessica Paczuski <jz.paczuski@gmail.com>
2016-04-12 16:07:35 +0200

    dont merge this, i dont know what i did

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160412122629) do
+ActiveRecord::Schema.define(version: 20160412133420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: :cascade do |t|
+    t.string   "date"
+    t.string   "time"
+    t.string   "place"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string   "subject"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tutors", force: :cascade do |t|
     t.string   "name"
