759e57956df4433e355ce2a72adba1a003f4025a
Jessica Paczuski <jz.paczuski@gmail.com>
2016-04-12 16:54:11 +0200

    add validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   has_many :subjects
   has_many :reservations
+
+ validates :username, presence: true
+validates :username, uniqueness: true
+
+validates :password, presence: true, length: { in: 5..20 }
+
+ validates :phone, allow_blank: true, length: { in: 5..20 }
+ validates :email, allow_blank: true, length: { in: 5..50 }
+
+ validates_format_of :phone, allow_blank: true, :with => /\A[+ 0-9]+$\z/
+ validates_format_of :email, allow_blank: true, :with => /@/, message: 'must contain @.'
+
 end
