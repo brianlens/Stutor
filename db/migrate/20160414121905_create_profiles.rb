236f5192bdeb184ff94ac285fddd39f0a2856a18
“Brian <brianlens@gmail.com>
2016-04-14 14:24:59 +0200

    added Profile model

---
+++ db/migrate/20160414121905_create_profiles.rb
@@ -0,0 +1,12 @@
+class CreateProfiles < ActiveRecord::Migration
+  def change
+    create_table :profiles do |t|
+      t.string :name
+      t.string :city
+      t.text :description
+      t.integer :price
+
+      t.timestamps null: false
+    end
+  end
+end
