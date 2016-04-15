d858c4baf28e34d5c71f162de5041157f6236d28
ReneeVL85 <renee.v.leijen@gmail.com>
2016-04-15 16:45:41 +0200

    login aanpassingen

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  # attr_accessible :email, :password, :username
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
   has_many :subjects
   has_many :reservations
   has_one :profile
+  # Virtual attribute for authenticating by either username or email
+  # This is in addition to a real persisted field like 'username'
 
 end
